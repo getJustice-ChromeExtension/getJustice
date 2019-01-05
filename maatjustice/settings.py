c13b3f6f1861819b166bef5b1992f0c01589e7dc
xXDuckXx <dacs2010@gmail.com>
2019-01-05 11:57:34 -0500

    creates app and updated settings

--- maatjustice/settings.py
+++ maatjustice/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # our apps
+    'core',
+
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
