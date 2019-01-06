d5774cd7c48368df78ad278e40e3ef7af60433f5
Sowmya <sowmya.aji@gmail.com>
2019-01-05 20:03:46 -0500

    add models User, Report and Tweet to models.py in core and set up auth_user_model = core.User in settings.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Report(models.Model):
+
+    title = models.CharField(max_length=250)
+
+    author = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="reports"
+    )
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Tweet(models.Model):
+
+    tweeter = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="tweets"
+    )
+    body = models.TextField()
