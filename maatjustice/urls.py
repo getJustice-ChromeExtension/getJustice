37b92a37dd09e79958914d207018fb784b4ec29a
tiana-horn <tiana.horn@gmail.com>
2019-01-08 04:21:11 -0500

    Deploy to Heroku

--- maatjustice/urls.py
+++ maatjustice/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from core import views
+from django.conf.urls.static import static
 
 
 urlpatterns = [
