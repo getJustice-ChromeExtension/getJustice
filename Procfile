b1004d289a4737e2f31dd5b31d7317d09b36b623
tiana-horn <tiana.horn@gmail.com>
2019-01-09 07:54:36 -0500

    Debug Heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-
 web: gunicorn maatjustice.wsgi
--- maatjustice/settings.py
+++ maatjustice/settings.py
@@ -16,6 +16,7 @@ import django_heroku
 
 load_dotenv()
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -163,11 +164,6 @@ STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 
 
 REST_FRAMEWORK = {
