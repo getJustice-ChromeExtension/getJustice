61a5e3147b89e4c11481d110b0a052229720a092
xXDuckXx <dacs2010@gmail.com>
2019-01-10 05:35:56 -0500

    takes screen shot and stores it in chromes local storage

--- get-justice-extension/popup.js
+++ get-justice-extension/popup.js
@@ -5,10 +5,11 @@ let screenshotButton = document.getElementById('screenshotButton');
 function takeScreenshot() {
     captureTab = chrome.tabs.captureVisibleTab(function (dataUrl) {
         console.log("success!")
-
+        chrome.storage.local.set({ "captured": dataUrl }, function () {
+            console.log('Value is set to ' + dataUrl);
+        });
     })
     takeScreenshot.onlclick = captureTab
-    
 }
 
 
