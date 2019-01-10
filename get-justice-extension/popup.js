a9c1b80655173055b8e862028d300081255f96d1
xXDuckXx <dacs2010@gmail.com>
2019-01-10 07:11:02 -0500

    storage working

--- get-justice-extension/popup.js
+++ get-justice-extension/popup.js
@@ -1,16 +1,37 @@
+// take screenshot, done
+// store screenshot, done
+// redirect to question.htlm
+// return screen shot 
+// attach to email
+
+
+/******* remove console logs dave !!! *******/
 console.log("extension connected")
 
 let screenshotButton = document.getElementById('screenshotButton');
 
+
 function takeScreenshot() {
     captureTab = chrome.tabs.captureVisibleTab(function (dataUrl) {
         console.log("success!")
-        chrome.storage.local.set({ "captured": dataUrl }, function () {
-            console.log('Value is set to ' + dataUrl);
+        chrome.storage.local.set({"captured": dataUrl}, function () {
+            console.log('screenshot: ' + dataUrl);
         });
     })
     takeScreenshot.onlclick = captureTab
 }
 
 
+// function downloadScreenshot() {
+//     // access bowser local storage
+//     // find the screenshot
+//     // then prompt user to download
+//     screenshot = chrome.storage.local.get({"captured": dataUrl})
+//     chrome.downloads.download(saveAs=true, screenshot)
+    
+// }
+
+// function attachScreenshot() {}
+
 takeScreenshot()
+downloadScreenshot()
