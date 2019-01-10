6ed285253308c5c9565883de8f09e26c860ed003
xXDuckXx <dacs2010@gmail.com>
2019-01-10 04:52:23 -0500

    cleans js

--- get-justice-extension/popup.js
+++ get-justice-extension/popup.js
@@ -1,5 +1,3 @@
-// 'use strict';
-
 console.log("extension connected")
 
 let screenshotButton = document.getElementById('screenshotButton');
@@ -14,21 +12,4 @@ function takeScreenshot() {
 }
 
 
-
 takeScreenshot()
-// let changeColor = document.getElementById('screenshotButton');
-
-// chrome.storage.sync.get('color', function (data) {
-//     changeColor.style.backgroundColor = data.color;
-//     changeColor.setAttribute('value', data.color);
-// });
-
-// changeColor.onclick = function (element) {
-//     console.log("hello")
-//     let color = element.target.value;
-//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//         chrome.tabs.executeScript(
-//             tabs[0].id,
-//             { code: 'document.body.style.backgroundColor = "' + color + '";' });
-//     });
-// };
