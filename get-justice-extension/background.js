3ccea0149f9518960c86ed7124bcd75c6a75e2f7
xXDuckXx <dacs2010@gmail.com>
2019-01-10 13:04:21 -0500

    extension opens new page with form in it

--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -3,5 +3,5 @@
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ color: '#3aa757' }, function () {
         console.log('The color is green.');
-    });
-});
+    })
+})
--- get-justice-extension/popup.js
+++ get-justice-extension/popup.js
@@ -1,32 +1,21 @@
-// return screen shot 
-// attach to email
-
-
 let screenshotButton = document.getElementById('screenshotButton');
+let postScreenshotButton = document.getElementById('sendButton')
 
 
 function takeScreenshot() {
     captureTab = function () {
         chrome.tabs.captureVisibleTab(function (dataUrl) {
-            chrome.storage.local.set({"captured": dataUrl}, function () {
-                console.log('screenshot: ' + dataUrl);
-            });
+            chrome.storage.local.set({"captured": dataUrl},function () {
+                console.log(dataUrl)
+            })
+            // open form.html
+            chrome.tabs.create({ "url": chrome.runtime.getURL("post.html")})
+            // change image src to data url
+            // submit form
         })
     }
     screenshotButton.addEventListener("click", captureTab)
 }
 
 
-// function downloadScreenshot() {
-//     // access bowser local storage
-//     // find the screenshot
-//     // then prompt user to download
-//     screenshot = chrome.storage.local.get({"captured": dataUrl})
-//     chrome.downloads.download(saveAs=true, screenshot)
-    
-// }
-
-// function attachScreenshot() {}
-
 takeScreenshot()
-// downloadScreenshot()
