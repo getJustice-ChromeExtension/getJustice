e5ea4a68149878cf49afc846c60438e37d424b41
xXDuckXx <dacs2010@gmail.com>
2019-01-11 04:12:56 -0500

    extension takes screenshot and then opens our create_report.html page

--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -1,11 +1,27 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.captureVisibleTab(function (dataUrl) {
-        chrome.tabs.create({url: chrome.runtime.getURL("post.html")}, function (tab) {
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//     chrome.tabs.captureVisibleTab({format: "png"}, function (dataUrl) {
+//         chrome.tabs.create({url: "create_report.html"}, function (tab) {
+//             chrome.tabs.executeScript(tab.id, {
+//                 code:`
+//                     let image = document.getElementById('post-form')
+//                     input.src = '${dataUrl}'
+//                 `
+//             })
+//         })
+//     })
+// })
+
+
+/* global chrome */
+
+chrome.browserAction.onClicked.addListener((tab) => {
+    chrome.tabs.captureVisibleTab({ format: 'png' }, function (dataUrl) {
+        chrome.tabs.create({ url: 'http://localhost:8000/create_report.html' }, function (tab) {
             chrome.tabs.executeScript(tab.id, {
-                code:`
-                    let image = document.getElementById('new-screenshot')
-                    input.src = '${dataUrl}'
-                `
+                code: `
+          var image = document.getElementById('image')
+          image.src = '${dataUrl}'
+        `
             })
         })
     })
