0b8017e6125bcbb7257cde2e074aca582225816c
xXDuckXx <dacs2010@gmail.com>
2019-01-11 05:52:48 -0500

    changes target url

--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -1,10 +1,12 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.captureVisibleTab({format: "png"}, function (dataUrl) {
-        chrome.tabs.create({ url: "http://14c39926.ngrok.io/"}, function (tab) {
+        chrome.tabs.create({ url: "http://localhost:8000/"}, function (tab) {
             chrome.tabs.executeScript(tab.id, {
                 code:`
                     let input = document.getElementById("new-screenshot")
                     input.src = '${dataUrl}'
+                    let submit = document.getElementById("sumbit-screenshot")
+                    submit.submit()
                 `
                 // need to add post request
             })
