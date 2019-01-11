981e61d7db25bfe4fc34ce2b5edde17ca0737a68
xXDuckXx <dacs2010@gmail.com>
2019-01-11 05:09:50 -0500

    takes screenshot, currently opens indexpage, and inserts screen shot into page

--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -1,9 +1,9 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.captureVisibleTab({format: "png"}, function (dataUrl) {
-        chrome.tabs.create({url: "http://localhost:8000/"}, function (tab) {
+        chrome.tabs.create({ url: "http://14c39926.ngrok.io/"}, function (tab) {
             chrome.tabs.executeScript(tab.id, {
                 code:`
-                    let image = document.getElementById('post-form')
+                    let input = document.getElementById("new-screenshot")
                     input.src = '${dataUrl}'
                 `
                 // need to add post request
