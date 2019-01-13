ef95c0c8d2436363562436c1fe383e9a997a6c7f
xXDuckXx <dacs2010@gmail.com>
2019-01-13 09:36:09 -0500

    redirects extension to create_report.html

--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.captureVisibleTab({format: "png"}, function (dataUrl) {
-        chrome.tabs.create({ url: "http://localhost:8000/"}, function (tab) {
+        chrome.tabs.create({ url: "http://127.0.0.1:8000/create-report/"}, function (tab) {
             chrome.tabs.executeScript(tab.id, {
                 code:`
                     let input = document.getElementById("new-screenshot")
