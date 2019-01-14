c0831eab51400d58484ccd6ac4cbaff1a578f92c
xXDuckXx <dacs2010@gmail.com>
2019-01-14 17:00:32 -0500

    subject buttons work

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,8 @@
-from django.forms import Form
+from django import forms
 
 
-class ReportForm(Form):
+class ReportForm(forms.Form):
+    subject = forms.CharField(max_length=200)
+    
     class Meta:
         fields = ('send_to', 'subject', 'message', 'screenshot')
