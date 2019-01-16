3950e76b0414b38e366d046f8ddf0887cebe0964
tiana-horn <tiana.horn@gmail.com>
2019-01-16 11:35:00 -0500

    Start with making extension rectangle like honey

--- get-justice-extension/popup.js
+++ get-justice-extension/popup.js
@@ -7,6 +7,17 @@
 // let screenshotButton = document.getElementById('screenshotButton');
 // let postScreenshotButton = document.getElementById('sendButton')
 
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.captureVisibleTab({format: "png"}, function (dataUrl) {
+        console.log(dataUrl)
+        chrome.tabs.create({ url: "http://127.0.0.1:8000/create-report"}, function (tab) {
+            chrome.tabs.executeScript(tab.id, {
+                code:`
+                    let input = document.getElementById("new-screenshot")
+                    input.src = '${dataUrl}'
+                    let imgData = document.getElementById("screenshot-data")
+                    imgData.setAttribute("value", "${dataUrl}")
+
 
 // function takeScreenshot() {
 //     captureTab = function () {
