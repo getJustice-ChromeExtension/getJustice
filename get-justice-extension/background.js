271a4cd9e8dfc29037530a920b318f916b195f56
Sowmya <sowmya.aji@gmail.com>
2019-01-17 18:07:59 -0500

    Add ability to close and open the extension form

--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -1,9 +1,9 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.captureVisibleTab({format: "png"}, function (dataUrl) {
+    chrome.tabs.captureVisibleTab({ format: "png" }, function (dataUrl) {
         console.log(dataUrl)
-        chrome.tabs.create({ url: "http://127.0.0.1:8000/create-report"}, function (tab) {
+        chrome.tabs.create({ url: "http://127.0.0.1:8000/report" }, function (tab) {
             chrome.tabs.executeScript(tab.id, {
-                code:`
+                code: `
                     let input = document.getElementById("new-screenshot")
                     input.src = '${dataUrl}'
                     let imgData = document.getElementById("screenshot-data")
