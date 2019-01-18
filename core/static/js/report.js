dff33f578fe4fb4dc862fdc4571dfb245a414d78
xXDuckXx <dacs2010@gmail.com>
2019-01-18 18:32:58 -0500

    subject btns work

--- core/static/js/report.js
+++ core/static/js/report.js
@@ -82,7 +82,7 @@ function addRemoveSubject() {
     buttons = document.querySelectorAll('.subject-button')
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            subject = event.target.getAttribute("data-type")
+            subject = button.getAttribute("data-type")
             subjectField = get('subject')
             if (subjectField.value.includes(`${subject}, `)) {
                 current = subjectField.value
@@ -101,7 +101,7 @@ function addRemoveEmailAddress() {
     buttons = document.querySelectorAll('.email-button')
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            newAddress = event.target.getAttribute("data-type")
+            newAddress = button.getAttribute("data-type")
             addressField = get('email-address')
             if (addressField.value.includes(`${newAddress}, `)) {
                 current = addressField.value
