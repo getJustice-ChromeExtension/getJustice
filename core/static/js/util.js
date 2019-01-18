bc3e6d9e2fba89bfb72199ffece43e1fcf074a02
xXDuckXx <dacs2010@gmail.com>
2019-01-18 08:20:38 -0500

    removes console logs

--- core/static/js/util.js
+++ core/static/js/util.js
@@ -3,11 +3,8 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="tooltip"], .tooltip').tooltip("hide")
-    console.log("connected");
-    console.log(button);
-    buttons = $('button');
+    buttons = $('button')
     buttons.click( function () {
-            console.log("click");
-            $('[data-toggle="tooltip"], .tooltip').tooltip("hide");
+            $('[data-toggle="tooltip"], .tooltip').tooltip("hide")
         });
 })
