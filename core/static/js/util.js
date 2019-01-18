48133a490716f25db2406e0a6546e3aa06ec91d4
xXDuckXx <dacs2010@gmail.com>
2019-01-18 08:17:58 -0500

    toottip shows on hover and dissapears on click

--- core/static/js/util.js
+++ core/static/js/util.js
@@ -1,31 +1,13 @@
 // //* tooltip function */
-// $(document).ready(function () {
-//     $('[data-toggle="tooltip"], .tooltip').tooltip();
-//     $('button').on("hover", function () {
-        
-//         $('[data-toggle="tooltip"], .tooltip').tooltip("show");
-//         $("button").click(function () {
-//             $('[data-toggle="tooltip"],.tooltip').tooltip("hide");
-//         });
-//     });
-// });
 
-// // $(function () {
-// //     $('[data-toggle="tooltip"]').tooltip()
-
-// // })
-
-
-$(document).ready(function () {
+$(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip()
-    console.log("connected")
-    console.log(button)
-    
-    $('button').each().on("mouseover", () => {
-    $('[data-toggle="tooltip"]').tooltip("show")  
-    })
-    // buttons.each().click( () => {
-    //     console.log("click")
-    //     $('[data-toggle="tooltip"], .tooltip').tooltip("hide")
-    // })
+    $('[data-toggle="tooltip"], .tooltip').tooltip("hide")
+    console.log("connected");
+    console.log(button);
+    buttons = $('button');
+    buttons.click( function () {
+            console.log("click");
+            $('[data-toggle="tooltip"], .tooltip').tooltip("hide");
+        });
 })
