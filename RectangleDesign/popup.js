5077b7210540af5dd98135badf93c09b8f5c4ed5
tiana-horn <tiana.horn@gmail.com>
2019-01-19 14:51:12 -0500

    Extension opening in New Window Fully Working

---
+++ RectangleDesign/popup.js
@@ -0,0 +1,171 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
+
+
+// let NewWindowButton = document.getElementById('NewWindowButton');
+
+//  function openNewWindow(url,windowName) {
+//     newwindow=window.open("http://127.0.0.1:8000/create-report", windowName,'height=200,width=150');
+//     if (window.focus) {newwindow.focus()}
+//     return false;
+//  }
+
+// openNewWindow()
+
+
+
+// need to write function that checks and unchecks boxes
+// need a function for subjects
+
+//* tooltip function */
+$(document).ready(function () {
+  $('button').on("hover", function () {
+      $('[data-toggle="tooltip"], .tooltip').tooltip();
+      $('[data-toggle="tooltip"], .tooltip').tooltip("show");
+      $("button").click(function () {
+          $('[data-toggle="tooltip"],.tooltip').tooltip("hide");
+      });
+  });
+});
+
+function get(id) {
+  object = document.getElementById(id)
+  return object
+}
+
+// if i click a subject then
+// the subject should have a class that is also on
+// the email fields
+// and should then fill the email fields
+
+
+function addRemoveHateCrimeAdresses() {
+  button = get('hate-crime-button')
+  button.addEventListener('click', () => {
+      emails = document.querySelectorAll('.email-button')
+      emails.forEach(email => {
+          if (email.classList.contains('hate-crime')) {
+              newEmail = email.getAttribute('data-type')
+              addressField = get('email-address')
+              if (addressField.value.includes(`${newEmail}, `)) {
+                  current = addressField.value
+                  edited = addressField.value.replace(`${newEmail}, `, '')
+                  addressField.value = edited
+              } else {
+                  current = addressField.value
+                  addressField.value = `${newEmail}, ${current}`
+              }
+          }
+      })
+  })
+}
+
+
+function addRemovePoliceBrutailityAdresses() {
+  button = get('police-brut-button')
+  button.addEventListener('click', () => {
+      emails = document.querySelectorAll('.email-button')
+      emails.forEach(email => {
+          if (email.classList.contains('police-brut')) {
+              newEmail = email.getAttribute('data-type')
+              addressField = get('email-address')
+              if (addressField.value.includes(`${newEmail}, `)) {
+                  current = addressField.value
+                  edited = addressField.value.replace(`${newEmail}, `, '')
+                  addressField.value = edited
+              } else {
+                  current = addressField.value
+                  addressField.value = `${newEmail}, ${current}`
+              }
+          }
+      })
+  })
+}
+
+
+function addRemoveCivilRightsAdresses() {
+  button = get('civ-rights-violate-button')
+  button.addEventListener('click', () => {
+      emails = document.querySelectorAll('.email-button')
+      emails.forEach(email => {
+          if (email.classList.contains('civ-rights')) {
+              newEmail = email.getAttribute('data-type')
+              addressField = get('email-address')
+              if (addressField.value.includes(`${newEmail}, `)) {
+                  current = addressField.value
+                  edited = addressField.value.replace(`${newEmail}, `, '')
+                  addressField.value = edited
+              } else {
+                  current = addressField.value
+                  addressField.value = `${newEmail}, ${current}`
+              }
+          }
+      })
+  })
+}
+
+
+function addRemoveSubject() {
+  buttons = document.querySelectorAll('.subject-button')
+  buttons.forEach(button => {
+      button.addEventListener('click', () => {
+          subject = event.target.getAttribute("data-type")
+          subjectField = get('subject')
+          if (subjectField.value.includes(`${subject}, `)) {
+              current = subjectField.value
+              edited = subjectField.value.replace(`${subject}, `, '')
+              subjectField.value = edited
+          } else {
+              current = subjectField.value
+              subjectField.value = `${subject}, ${current}`
+          }
+      })
+  })
+}
+
+
+function addRemoveEmailAddress() {
+  buttons = document.querySelectorAll('.email-button')
+  buttons.forEach(button => {
+      button.addEventListener('click', () => {
+          newAddress = event.target.getAttribute("data-type")
+          addressField = get('email-address')
+          if (addressField.value.includes(`${newAddress}, `)) {
+              current = addressField.value
+              edited = addressField.value.replace(`${newAddress}, `, '')
+              addressField.value = edited
+          } else {
+              current = addressField.value
+              addressField.value = `${newAddress}, ${current}`
+          }
+      })
+  })
+}
+
+
+
+get()
+addRemoveEmailAddress()
+addRemoveSubject()
+addRemoveHateCrimeAdresses()
+addRemovePoliceBrutailityAdresses()
+addRemoveCivilRightsAdresses()
--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -12,7 +12,67 @@ chrome.browserAction.onClicked.addListener(function (tab) {
             })
         })
     })
-})
+});
+
+
+
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    /* Get the `tab`'s window along with its containing tabs */
+    chrome.windows.get(tab.windowId, { populate: true }, function(oldWin) {
+        /* Determine which tabs should be moved
+         * (i.e. are on the left of `tab` */
+        var tabs = oldWin.tabs;
+        var tabsToMove = [];
+        for (var i = 0; i < tabs.length; i++) {
+            if (tabs[i].index === tab.index) {
+                tabsToMove.push(tabs[i].id);
+            }
+        }
+
+        /* If there are any tabs to move... */
+        if (tabsToMove.length > 0) {
+            /* Create a new window with the same
+             * location and size as the original */
+            chrome.windows.create({
+                top: oldWin.top,
+                left: oldWin.left,
+                width: oldWin.width,
+                height: oldWin.height,
+                focused: false
+            }, function(newWin) {
+                /* Remove the new, empty tab created by default */
+                chrome.tabs.query({
+                    windowId: newWin.id
+                }, function(tabsToClose) {
+                    /* Update the window's state (e.g. "maximized") */
+                    chrome.windows.update(newWin.id, { state: oldWin.state });
+
+                    /* Move the tabs to the newly created window */
+                    chrome.tabs.move(tabsToMove, {
+                        windowId: newWin.id,
+                        index: -1
+                    }, function() {
+                        /* Close any tabs that pre-existed (i.e. 1 empty tab)
+                         * [Do not do this BEFORE moving the tabs,
+                         *  or the window will be empty and will close] */
+                        var lastIdx = tabsToClose.length - 1;
+                        tabsToClose.forEach(function(t, idx) {
+                            chrome.tabs.remove(t.id);
+                            if (idx === lastIdx) {
+                                chrome.windows.update(oldWin.id, {
+                                    focused: true
+                                });
+                            }
+                        });
+                    });
+                });
+            });
+        }
+    });
+});
+
 
 
 
