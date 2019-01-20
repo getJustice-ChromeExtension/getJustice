06f5ea987a0b11fba901d54a6571c93809a71d2e
tiana-horn <tiana.horn@gmail.com>
2019-01-20 15:17:01 -0500

    Distinguish production and dev email settings. Dev turned on

--- maatjustice/settings.py
+++ maatjustice/settings.py
@@ -194,20 +194,20 @@ django_heroku.settings(locals())
 #Email Settings
 
 #SendGrid Email Settings (Sends email to real people)
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.getenv("Justice_Key")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = os.getenv("Justice_Key")
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 #MailDev Email Settings (Doesn't send email to real people)
-# if DEBUG:
-    # EMAIL_HOST = '127.0.0.1'
-    # EMAIL_HOST_USER = ""
-    # EMAIL_HOST_PASSWORD = ""
-    # EMAIL_PORT = 1025
-    # EMAIL_USE_TLS = False
-    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+if DEBUG:
+    EMAIL_HOST = '127.0.0.1'
+    EMAIL_HOST_USER = ""
+    EMAIL_HOST_PASSWORD = ""
+    EMAIL_PORT = 1025
+    EMAIL_USE_TLS = False
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
