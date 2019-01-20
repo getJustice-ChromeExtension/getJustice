342e61b7505b67902234189e70fd7956953148a1
tiana-horn <tiana.horn@gmail.com>
2019-01-20 14:09:30 -0500

    Extension Links to Heroku Site

--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -1,7 +1,9 @@
+
+
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.captureVisibleTab({ format: "png" }, function (dataUrl) {
         console.log(dataUrl)
-        chrome.tabs.create({ url: "http://127.0.0.1:8000/ext-report/" }, function (tab) {
+        chrome.tabs.create({ url: "https://getjustice.herokuapp.com/ext-report/" }, function (tab) {
             chrome.tabs.executeScript(tab.id, {
                 code: `
                     let input = document.getElementById("new-screenshot")
@@ -62,6 +64,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         }
     });
 });
+
 
 
 
