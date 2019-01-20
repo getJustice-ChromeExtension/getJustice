69557f2ff01286edd31b35fb9bfa3d3c17cc6829
tiana-horn <tiana.horn@gmail.com>
2019-01-20 07:45:55 -0500

    Extension Report opens with screenshot in correct location without bringing tabs from active window

--- get-justice-extension/background.js
+++ get-justice-extension/background.js
@@ -15,21 +15,17 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    /* Get the `tab`'s window along with its containing tabs */
     chrome.windows.get(tab.windowId, { populate: true }, function(oldWin) {
-        /* Determine which tabs should be moved */
         var tabs = oldWin.tabs;
         var tabsToMove = [];
         for (var i = 0; i < tabs.length; i++) {
-            if (tabs[i].index === tab.index) {
-                tabsToMove.push(tabs[i].id);
+            if (tabs[i].index < -1) {
+                tabsToMove.shift(tabs[i].id);
+            } else {
+                tabsToMove.push(tabs[i].id)
             }
         }
-
-        /* If there are any tabs to move... */
         if (tabsToMove.length > 0) {
-            /* Create a new window with the same
-             * location and size as the original */
             chrome.windows.create({
                 top: oldWin.top,
                 left: oldWin.left,
@@ -37,22 +33,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
                 height: oldWin.height,
                 focused: false
             }, function(newWin) {
-                /* Remove the new, empty tab created by default */
                 chrome.tabs.query({
                     windowId: newWin.id
                 }, function(tabsToClose) {
-                    /* Update the window's state (e.g. "maximized") */
                     chrome.windows.update(newWin.id, { state: oldWin.state });
-
-                    /* Move the tabs to the newly created window */
                     chrome.tabs.move(tabsToMove, {
                         windowId: newWin.id,
                         index: -1,
-
                     }, function() {
-                        /* Close any tabs that pre-existed (i.e. 1 empty tab)
-                         * [Do not do this BEFORE moving the tabs,
-                         *  or the window will be empty and will close] */
+
                         var lastIdx = tabsToClose.length - 1;
                         tabsToClose.forEach(function(t, idx) {
                             chrome.tabs.remove(t.id);
@@ -62,8 +51,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
                                     left: (oldWin.width)-400,
                                     width: 200,
                                     height: 700,
-                                    // height: (oldWin.height)-100,
-                                    // height: (oldWin.height)-105,
                                     focused: true
                                 });
                             }
