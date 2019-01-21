fda1cd976d69abcf3347881bf4349e1cb8644573
tiana-horn <tiana.horn@gmail.com>
2019-01-21 17:00:50 -0500

    Email Settings

--- maatjustice/settings.py
+++ maatjustice/settings.py
@@ -202,12 +202,19 @@ django_heroku.settings(locals())
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 #MailDev Email Settings (Doesn't send email to real people)
-if DEBUG:
-    EMAIL_HOST = '127.0.0.1'
-    EMAIL_HOST_USER = ""
-    EMAIL_HOST_PASSWORD = ""
-    EMAIL_PORT = 1025
-    EMAIL_USE_TLS = False
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# if DEBUG:
+#     EMAIL_HOST = '127.0.0.1'
+#     EMAIL_HOST_USER = ""
+#     EMAIL_HOST_PASSWORD = ""
+#     EMAIL_PORT = 1025
+#     EMAIL_USE_TLS = False
+#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+#MailGun
+
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = 'sandboxb913aef2e2364d4292ac548befbb1a04.mailgun.org'
+EMAIL_HOST_PASSWORD = os.getenv('MAILGUN_PASS')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
