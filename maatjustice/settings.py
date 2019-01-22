c9c57cb1159ec97347d4ffd82b27137407d74fc6
Tiana Horn <44574217+tiana-horn@users.noreply.github.com>
2019-01-22 13:40:14 -0500

    Update settings.py

--- maatjustice/settings.py
+++ maatjustice/settings.py
@@ -102,7 +102,7 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-SITE_ID = 1
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 # LOGIN_REDIRECT_URL = 'ext_report'
