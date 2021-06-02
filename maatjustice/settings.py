fa674623ef7ca2c01e756422b177e4b1fbd46aac
Tiana Horn <44574217+tiana-horn@users.noreply.github.com>
2021-06-01 22:05:41 -0400

    Update settings.py
    Remove ability to send emails to real people

--- maatjustice/settings.py
+++ maatjustice/settings.py
@@ -197,19 +197,19 @@ django_heroku.settings(locals())
 # Email Settings
 
 # MailGun (Sends to real people)
-EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'postmaster@get-justice.org'
-EMAIL_HOST_PASSWORD = os.getenv('MAILGUN_PASS')
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.mailgun.org'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'postmaster@get-justice.org'
+# EMAIL_HOST_PASSWORD = os.getenv('MAILGUN_PASS')
+# EMAIL_USE_TLS = True
 
 # MailDev Email Settings (Doesn't send email to real people)
 # if DEBUG:
-#     EMAIL_HOST = '127.0.0.1'
-#     EMAIL_HOST_USER = ""
-#     EMAIL_HOST_PASSWORD = ""
-#     EMAIL_PORT = 1025
-#     EMAIL_USE_TLS = False
-#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = '127.0.0.1'
+    EMAIL_HOST_USER = ""
+    EMAIL_HOST_PASSWORD = ""
+    EMAIL_PORT = 1025
+    EMAIL_USE_TLS = False
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ACCOUNT_ACTIVATION_DAYS = 7
